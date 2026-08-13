6cf63993804af8cbbfed61ae67a3fece1bd11785
大爷 赵 <1509157464@qq.com>
2026-08-13 15:05:04 +0800

    ci: 修正 workflowPresets linux环境下执行错误

--- tests/pch.h
+++ tests/pch.h
@@ -1,4 +1,5 @@
-﻿#pragma once
+#ifndef POSTANVIL_TESTS_PCH_H
+#define POSTANVIL_TESTS_PCH_H
 
 #include "TestFramework.hpp"
 #include <PostAnvil.h>
@@ -10,3 +11,5 @@
 #include <string_view>
 #include <utility>
 #include <vector>
+
+#endif
